da2453a5a8c7d436c68a98b8ab9032c71c3cf8ab
weidaru <weidaru@gmail.com>
2014-08-14 17:23:44 -0700

    Worked more on script.

--- engine/script/generate_type_info.lua
+++ engine/script/generate_type_info.lua
@@ -1,8 +1,21 @@
 local option_parser = require("option_parser")
 
+local function get_os() 
+	if package.config:sub(1,1)=="/" then
+		return "unix"
+	elseif package.config:sub(1,1)=="\\" then
+		return "windows"
+	end
+end
+
 local function scan(dir, func) 
-	local f = io.popen(string.format("find %s -name \"*.h\" -or -name \"*.hpp\" -or -name \"*.cpp\" -or -name \"*.hcc\""))
-	f = f or io.popen(string.format("dir %s *.h *.hpp *.hh *.cpp *.cc /s/b", dir))
+	local f=nil
+	if get_os()=="windows" then
+		f = io.popen(string.format("dir %s\\*.h %s\\*.hpp %s\\*.cpp %s\\*.cc /s/b/a-s", dir, dir, dir, dir)) 
+	elseif get_os()=="unix" then
+		f = io.popen(string.format("find %s -name \"*.h\" -or -name \"*.hpp\" -or -name \"*.cpp\" -or -name \"*.cc\"", dir))
+	end
+	
 	assert(f, "Cannot list all the related files")
 	
 	for line in f:lines() do
@@ -32,6 +45,20 @@ assert(dest~=nil, "Dest Dir is nil!")
 
 --Start scan
 local buffer = {}
-scan(source_dir, function(filepath)
-	
+scan(source_dir, 
+function(filepath)
+	local f = assert(io.open(filepath, "r"))
+	--Read line by line
+	for line in f:lines() do
+		local annotation = line:match("[%s\t]*//%[%[[%s\t]*(%w+)[%s\t]*%]%]//[%s\t\n]*")
+		if annotation == "TypeInfo" then
+			print(filepath)
+		end
+	end
+	f:close()
 end)
+
+
+
+
+
