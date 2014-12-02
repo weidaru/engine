febf4b9d8800ba8834abdbca5ebf9b51ba96bc2b
weidaru <weidaru@gmail.com>
2014-12-01 21:04:39 -0800

    Removed all directx related header in test_program.

--- engine/src/engine_program_imp/test_program.cpp
+++ engine/src/engine_program_imp/test_program.cpp
@@ -1,17 +1,3 @@
-#pragma comment(lib, "dxgi.lib")
-#pragma comment(lib, "d3d11.lib")
-#pragma comment(lib, "d3dx11.lib")
-#pragma comment(lib, "d3dcompiler.lib")
-#pragma comment(lib, "dxguid.lib")
-
-
-#define WIN32_LEAN_AND_MEAN
-#include <dxgi.h>
-#include <d3d11.h>
-#include <d3d11shader.h>
-#include <d3dcompiler.h>
-#undef ERROR
-
 #include <glog/logging.h>
 
 #include "engine_program.h"
